160009a86d3041ad6039fcadfce2cff6f467916e
ahahtyler <tyler.stigliano@verizon.com>
2016-10-18 16:38:28 -0400

    Update threshold-alert.rb

--- lib/logstash/outputs/threshold-alert.rb
+++ lib/logstash/outputs/threshold-alert.rb
@@ -3,109 +3,129 @@ require "logstash/outputs/base"
 require "logstash/namespace"
 
 # An example output that does nothing.
-class LogStash::Outputs::Threshold-Alert < LogStash::Outputs::Base
+class LogStash::Outputs::ThresholdAlert < LogStash::Outputs::Base
   config_name "threshold_alert"
 
-  config :to, :validate => :string, :required => true
-  config :from, :validate => :string, :default => "logstash.alert@nowhere.com"
-  config :replyto, :validate => :string
-  config :cc, :validate => :string
-  config :via, 			  :validate => :string,  :default => "smtp"
-  config :address,  	  :validate => :string,  :default => "localhost"
-  config :port, 		  :validate => :number,  :default => 25
-  config :domain, 		  :validate => :string,  :default => "localhost"
-  config :username, 	  :validate => :string
-  config :password, 	  :validate => :string
-  config :authentication, :validate => :string
-  config :use_tls, 		  :validate => :boolean, :default => false
-  config :debug, 		  :validate => :boolean, :default => false
-  config :subject, 		  :validate => :string,  :default => ""
-  config :body, 		  :validate => :string,  :default => ""
-  config :htmlbody, 	  :validate => :string,  :default => ""
-  config :attachments, 	  :validate => :array,   :default => []
-  config :contenttype, 	  :validate => :string,  :default => "text/html; charset=UTF-8"
-  
-  
+  config :to,                 :validate => :string,   :required => true
+  config :from,               :validate => :string,   :default => "logstash.alert@nowhere.com"
+  config :cc,                 :validate => :string
+  config :via, 			          :validate => :string,   :default => "smtp"
+  config :address,  	        :validate => :string,   :default => "localhost"
+  config :port, 		          :validate => :number,   :default => 25
+  config :domain, 		        :validate => :string,   :default => "localhost"
+  config :username, 	        :validate => :string
+  config :password, 	        :validate => :string
+  config :authentication,     :validate => :string
+  config :use_tls, 		        :validate => :boolean,  :default => false
+  config :debug, 		          :validate => :boolean,  :default => false
+  config :subject, 		        :validate => :string,   :default => ""
+  config :body, 		          :validate => :string,   :default => ""
+
+  config :threshold_time,     :validate => :number,   :default => 600
+  config :threshold_events,   :validate => :number,   :default => 100
+
+  config :jira_key,           :validate => :string
+  config :jira_url,           :validate => :string
+  config :jira_user,          :validate => :string
+  config :jira_password,      :validate => :string
+
+  config :smsnumber,          :validate => :string
+  config :smsprovider,        :validate => :string
+
+  config :shell_command,      :validate => :string
+  config :shell_script_path,  :validate => :string
+
   public
   def register
-  
-	require "mail"
+
+    require "mail"
+
+    @email = false
+    @sms   = false
+    @jira  = false
+    @shell = false
 
     options = {
-      :address              => @address,
-      :port                 => @port,
-      :domain               => @domain,
-      :user_name            => @username,
-      :password             => @password,
-      :authentication       => @authentication,
-      :enable_starttls_auto => @use_tls,
-      :debug                => @debug
+        :address              => @address,
+        :port                 => @port,
+        :domain               => @domain,
+        :user_name            => @username,
+        :password             => @password,
+        :authentication       => @authentication,
+        :enable_starttls_auto => @use_tls,
+        :debug                => @debug
     }
 
-    if @via == "smtp"
-      Mail.defaults do 
-        delivery_method :smtp, options
-      end
-    elsif @via == 'sendmail'
-      Mail.defaults do
-        delivery_method :sendmail
-      end
-    else
-      Mail.defaults do
-        delivery_method :@via, options
-      end
-    end # @via tests
+    Mail.defaults { delivery_method :smtp, options } if @via == "smtp"
+    Mail.defaults { delivery_method :sendmail }      if @via == 'sendmail'
+    Mail.defaults { delivery_method :@via, options } unless @via == 'smtp' && @via == 'snedmail'
+
     @logger.debug("Email Output Registered!", :config => options, :via => @via)
-	
+
+    @start_time = Time.now.to_i
+    @total_time = 0
+    @event_counter = 0
+    @total_events = 0
+
   end # def register
 
   public
   def receive(event)
-  
-	#find the threshold
-	## If the number of logs you've received goes below a threshold over a certain period of time
-	## throw up an alert. 
-	
-	#Send email
-	  @logger.debug? and @logger.debug("Creating mail with these settings : ", :via => @via, :options => @options, :from => @from, :to => @to, :cc => @cc, :subject => @subject, :body => @body, :content_type => @contenttype, :htmlbody => @htmlbody, :attachments => @attachments, :to => to, :to => to)
-	  formatedSubject = event.sprintf(@subject)
-	  formattedBody = event.sprintf(@body)
-	  formattedHtmlBody = event.sprintf(@htmlbody)
-	  mail = Mail.new
-	  mail.from = event.sprintf(@from)
-	  mail.to = event.sprintf(@to)
-	  if @replyto
-		mail.reply_to = event.sprintf(@replyto)
-	  end
-	  mail.cc = event.sprintf(@cc)
-	  mail.subject = formatedSubject
-	  if @htmlbody.empty?
-		formattedBody.gsub!(/\\n/, "\n") # Take new line in the email
-		mail.body = formattedBody
-	  else
-		mail.text_part = Mail::Part.new do
-		  content_type "text/plain; charset=UTF-8"
-		  formattedBody.gsub!(/\\n/, "\n") # Take new line in the email
-		  body formattedBody
-		end
-		mail.html_part = Mail::Part.new do
-		  content_type "text/html; charset=UTF-8"
-		  body formattedHtmlBody
-		end
-	  end
-	  @attachments.each do |fileLocation|
-		mail.add_file(fileLocation)
-	  end # end @attachments.each
-	  @logger.debug? and @logger.debug("Sending mail with these values : ", :from => mail.from, :to => mail.to, :cc => mail.cc, :subject => mail.subject)
-	  begin
-		mail.deliver!
-	  rescue StandardError => e
-		@logger.error("Something happen while delivering an email", :exception => e)
-		@logger.debug? && @logger.debug("Processed event: ", :event => event)
-	  end
-	  
-	#Create jira ticket
-  
+
+    @event_counter += 1
+
+    if (@start_time + @threshold_time) <= Time.now.to_i
+
+      if @event_counter <= threshold_events
+
+        send_email    if @email
+        send_jira     if @jira
+        execute_shell if @shell
+        sned_sms      if @sms
+
+      end
+
+      @total_events += @event_counter
+      @total_execution_time += @threshold_time
+      @event_counter = 0
+      @start_time = Time.now.to_i
+
+    end
+
     return "Event received"
   end # def event
+
+  public
+  def send_email
+
+    mail = Mail.new
+    mail.from = @from
+    mail.to = @to
+    mail.cc = @cc
+    mail.subject = @subject
+    mail.body = @body.gsub!(/\\n/, "\n")
+
+    begin
+      mail.deliver!
+    rescue StandardError => e
+      @logger.error("Something happen while delivering an email", :exception => e)
+    end
+
+  end
+
+  public
+  def send_jira
+
+  end
+
+  public
+  def execute_shell
+
+  end
+
+  pubilc
+  def send_sms
+
+  end
+
 end # class LogStash::Outputs::Example
