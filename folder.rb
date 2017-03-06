91e16302ee27f3a6e571cf925ca7bdf0895617bd
Tyler Stigliano <tyler.stigliano@verizon.com>
2017-03-05 21:35:58 -0800

    Create folder.rb

---
+++ folder.rb
@@ -0,0 +1,16 @@
+class Folder
+  attr_accessor :guid, :name, :group
+
+  def initialize(guid, name, group)
+    @guid  = guid.to_s
+    @name  = name.to_s
+    @group = group
+  end
+
+  def has_parent?(maxis)
+    #folder = maxis.get("/folders/#{@guid}")
+    #folder['parent'].nil? ? true : false
+    true
+  end
+
+end
