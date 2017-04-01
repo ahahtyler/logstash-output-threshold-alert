846953d7b4675710961bbfc606167f3373117a71
Tyler Stigliano <tyler.stigliano@verizon.com>
2017-03-31 23:02:59 -0700

    Update outline.rb

--- outline.rb
+++ outline.rb
@@ -7,110 +7,136 @@ require_relative 'lib/maxis_connection.rb'
 require 'rest-client'
 require 'json'
 
-TODAY = Time.parse(DateTime.now.to_s)
-YESTERDAY = Time.parse((DateTime.now - 7).to_s)
-
-puts TODAY
-puts YESTERDAY
-
-materialSet = 'com.amazon.credentials.isengard.968648960172.user/stigty'
-credentials = Aws::OdinCredentials.new(materialSet)
-sim = Amazon::SIM.new( :region => 'us-west-2',
-					   :access_key_id => credentials.access_key_id,
-					   :secret_access_key => credentials.secret_access_key)
-
-issues = sim.issues.filters(:containingFolder => ["f8f2a971-ecdb-4551-aed1-744f9204d378"],
-							:createDate => [YESTERDAY, TODAY])
-
-count = 0
-new_issues = Array.new
-issues.each do |i|
-	puts "ID #{i.id}"
-	puts "Created: #{i.created}"
-	puts "Folder: #{i.folder.name}"
-	puts "Submitter: #{i.submitter}"
-	es_data_point ={ 'issue_id'      => i.id ,
-                     'folder_name'   => "" ,
-                     'folder_guid'   => "" ,
-					 'touched'       => false,
-					 'first_touched' => "" ,
-					 'create_date'   => i.created,
-					 'touch_time'    =>  "" ,
-                     '@timestamp'    => DateTime.now}
-	
-	new_issues.push(es_data_point)
-	break;
+def initialize_sim_api
+  materialSet = 'com.amazon.credentials.isengard.968648960172.user/stigty'
+  credentials = Aws::OdinCredentials.new(materialSet)
+  return Amazon::SIM.new( :region            => 'us-west-2',
+                          :access_key_id     => credentials.access_key_id,
+                          :secret_access_key => credentials.secret_access_key)
 end
 
-@es_server   = "supbtsearch.corp.amazon.com"
-@es_index    = "count_metrics_test"
+def create_team_list
+  teamHash = Hash.new()
 
-elastic = Elasticsearch.new(@es_server)
-#TODO: Write if not duplicate
-elastic.write_to_index(@es_index, new_issues)
+  team_block = @doc.css("team_config/service_line")
+  team_block.map do |service|
+    subfolderHash = Hash.new()
+    service.css("folder_group").map do |subfolder|
+      guidArray = Array.new()
+      subfolder.css("guid").map do |guid|
+        guidArray.push(guid.children.to_s)
+      end
+      subfolderHash[subfolder["name"]] = guidArray
+    end
 
-elastic_url = "http://#{@es_server}/#{@es_index}/_search?q=touched:false"
-response    = RestClient.get(elastic_url, "Content-Type" => "application/json")
-response = JSON.parse(response)['hits']['hits']
+    ignoreFolderArrray = Array.new()
+    ignoreLabelsArray = Array.new()
+
+    service.css("ignore").map do |item|
+      ignoreFolderArrray.push(item["guid"]) if item["type"].eql?("folder")
+      ignoreLabelsArray.push(item["guid"])  if item["type"].eql?("label")
+    end
+    teamHash[service["name"]] = {'groups' => subfolderHash, 'ignoreF' => ignoreFolderArrray, 'ignoreL' => ignoreLabelsArray}
+  end
+  return teamHash
+end
+
+def generate_es_input(issues)
+  new_issues = Array.new
+  issues.each do |i|
+    es_data_point ={ 'issue_id'      => i.id ,
+                     'create_date'   => i.created,
+                     'touched'       => false,
+                     '@timestamp'    => @today,
+                     'folder_name'   => "",
+                     'first_touched' => "",
+                     'time_open'    => ""}
+    #maybe add a label here
+    new_issues.push(es_data_point)
+  end
+  return new_issues
+end
+
+def get_es_results(index, payload)
+  elastic_url = "http://#{@es_server}/#{index}/_search?q=#{payload}"
+  response    = RestClient.get(elastic_url, "Content-Type" => "application/json")
+  response = JSON.parse(response)['hits']['hits']
+  return response
+end
+
+def update_es_item(index, key, value, id)
+  elastic_url = "http://#{@es_server}/#{index}/metric/#{id}/_update"
+  puts elastic_url
+  payload = {
+      "doc" => {
+          key => value
+      }
+  }
+  response = RestClient.post(@elastic_url, payload.to_json, "Content-Type" => "application/json")
+end
+
+def find_resolvers(teamHash)
+  {"CS Guid" => ["stigty"],
+   "ACDC guid" => ["stigty"] }
+  #for each team
+    #for each folder
+      #Find resolvers on folder
+      #Find members on resolver group
+    #end
+  #end
+end
 
 @host        = 'maxis-service-prod-pdx.amazon.com'
 @scheme      = 'https'
 @region      = 'us-west-2'
 @materialSet = 'com.amazon.credentials.isengard.968648960172.user/stigty'
-@maxis = MaxisConnection.new(@host, @scheme, @region, @materialSet)
 
-response.each do |r|
-	issue_id = r["_source"]["issue_id"]
-	puts issue_id
-	edits = @maxis.get("/issues/#{issue_id}/edits")
-	edits["edits"].each do |e|
-		puts "Actor: #{e["actualOriginator"]}"
-		puts "Time: #{e["actualCreateDate"]}"
-		e["pathEdits"].each do |p|
-			puts "---Action: #{p["path"]}"
-		end
-	end
-	
-	elastic_url = "http://#{@es_server}/#{@es_index}/metric/#{r["_id"]}/_update"
-	puts elastic_url
-	payload = {
-				"doc" => {
-							"touched" => true
-						 }
-				}
-	
-	response = RestClient.post(elastic_url, payload.to_json, "Content-Type" => "application/json")	
-	puts response
-	break;
+@es_server = "supbtsearch.corp.amazon.com"
+@es_index  = "count_metrics_test"
+
+@today     = Time.parse(DateTime.now.strftime("%Y-%m-%dT08:00:00.000Z").to_s)
+@yesterday = Time.parse((DateTime.now - 1).strftime("%Y-%m-%dT08:00:00.000Z").to_s)
+
+maxis   = MaxisConnection.new(@host, @scheme, @region, @materialSet)
+elastic = Elasticsearch.new(@es_server)
+
+teamHash     = create_team_list
+resolverHash = find_resolvers(teamHash)
+sim_conn     = sintialize_sim_api
+
+#for each team
+  #for each folder
+   issues = sim_conn.issues.filters(:containingFolder => [folder.guid],
+                                    :createDate       => [@yesterday, @today])
+   issue_items = generate_es_input(issues)
+  #end
+#end
+
+elastic.write_to_index(@es_index, issue_items)
+es_results = get_es_results(@es_index, "touched:false")
+
+es_results.each do |r|
+  issue_id = r["_source"]["issue_id"]
+  edits = maxis.get("/issues/#{issue_id}/edits")
+  edits["edits"].each do |e|
+    puts "Actor: #{e["actualOriginator"]}"
+    puts "Time: #{e["actualCreateDate"]}"
+    e["pathEdits"].each do |p|
+      puts "---Action: #{p["path"]}"
+      #Get folder GUID
+
+      #if path == comment && resolver["GUID"].include?(actorOriginator)
+        update_es_item(@es_index,"touched", "true", r["_id"])
+        update_es_item(@es_index, "folder_name", r["folder"], r["_id"])
+        update_es_item(@es_index, "first_touched", ftt, r["_id"])
+        update_es_item(@es_index, "open_time", time_in_sec, r["_id"])
+      #else
+         #figure out how long ticket has been open and update open_time
+      #end
+
+    end
+  end
+
+
 end
 
-
-
-#Query ES for non-touched issues
-#Loop through Nontouched issues
-  # Query SIM for edit history
-  # Query SIM/TT for resolver groups
-  # Loop through edit history
-    # Find create date
-    # Find comment date by resolver
-    # If comment dat by resolver is found
-      # Update ES item touched = true
-
-
-#Store
-# {
-#   issue_id      - String
-#   issue_guid    - String
-#   folder_name   - String
-#   folder_guid?  - String
-#   touched       - boolean
-#   first_touch   - DateTime
-#   create_date   - DateTime
-#   touch_time    - Int (minutes)
-#   @timestamp    - DateTime
-# }
-
-
-###ES Methods
-#Query(index, payload)
-#Update item(index, payload, itemNumber)
